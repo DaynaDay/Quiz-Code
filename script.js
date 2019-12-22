35b3eb387b9dc0014f76d603751fb3f7333b3764
Dayna Knight <dknighted11@gmail.com>
2019-12-21 22:44:45 -0500

    set the time interval, major functions, and altering the read me.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
   var postGame = document.querySelector("post-game-screen");
   var userScore = document.querySelector("user-score");
   var playAgain = document.querySelector("play-again-btn");
-
+  var questionIndex= document.querySelector("data-question-index");
 var questions
  var timerId 
 
@@ -25,9 +25,8 @@ var questions
 
 
 // create function to start game
-startButton.addEventListener("click") {
-  secondsLeft = 
-}
+startButton.addEventListener("click") 
+  secondsLeft = setInterval(timerId, 30000)
   // set secondsLeft variable starting time (300 seconds = 5 minutes)
   // write secondsLeft to the page
 
@@ -52,44 +51,71 @@ startButton.addEventListener("click") {
     }
   }, 1000);
 
-
-
 // create function to display a question and possible choices
 function displayQuestion(questionIndex) {
+  
   // check if questionIndex in questions array doesn't exist
   if (!questions[questionIndex]) {
     // stop game, we've hit last question
     return stopGame();
+  };
+  
+  // print question to the page
+  questionIndex.textContent.append("body");
+  // use data attribute to know which index the question is
+ questions.getAttribute("data-question-index");
+  // loop through choices and print out choices to the page (make them buttons)
+  for (var i = 0; i < questions.length; i ++); {
+  button = questions.textContent("button");
   }
 
-  // get questions[questionIndex]
-  // print question to the page
-  // use data attribute to know which index the question is
-  // loop through choices and print out choices to the page (make them buttons)
-
-}
+};
 
 // create function to handle users answering
-  // use event delegation to make sure button was clicked
-  // read data attribute of what question we answered (index)
-  // check to see if choice picked is same as questions correct answer
-  // if yes, increase score++
-  // if no, subtract time from secondsLeft
+function userAnswerHandle (){
+// use event delegation to make sure button was clicked
+button.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (!choices === answer){
+    return (userScore = secondsLeft - 100);
+  }
+  else {
+    userScore = secondsLeft ++; 
+  };
   
   // get index of next question (this question's index + 1)
+nextQuestionIndex = questionIndex +1;
   // run displayQuestion(nextQuestionIndex)
 
+displayQuestion(nextQuestionIndex)
+};
 
+
+  
 
 // create a function to stop the game (either by answering all the questions or time has run out)
+function endGame (){
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      quizContent.classList.remove("hide");
+      postGame.classList.add("hide");
+
+
+}
   // clearInterval() to stop the timer
   // hide quiz-content element
   // show post-game-screen
   // print out user score
 
-
+renderGame();
 
 // add event listeners
   // start game button (for starting the game)
+  startButton.addEventListener("click", function);
   // quizcontent (for answering a question) -> use event delegation
-  // play again button (for starting the game)
+  quizContent.addEventListener.event.target.
+  // play again button (for starting the game);
+playAgain.addEventListener("click", function);
